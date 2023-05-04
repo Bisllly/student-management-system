e3a8b335d8568339d635913f8209f98f18122441
cuongnguyen-oceansmart <cuongnguyen3900@gmail.com>
2023-05-04 14:28:23 +0700

    Initial Commit

---
+++ src/main/java/com/example/studentmanagementsystem/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.studentmanagementsystem.repository;
+
+import com.example.studentmanagementsystem.entity.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends MongoRepository<Student, Long> {
+
+}
---
+++ src/main/java/com/example/studentmanagementsystem/service/impl/StudentServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.studentmanagementsystem.service.impl;
+
+import com.example.studentmanagementsystem.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+}
