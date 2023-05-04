970ac37c288d5bb64d4326ff55b762832469686c
cuongnguyen-oceansmart <cuongnguyen3900@gmail.com>
2023-05-04 17:46:10 +0700

    Initial Commit

--- src/main/java/com/example/studentmanagementsystem/service/StudentService.java
+++ src/main/java/com/example/studentmanagementsystem/service/StudentService.java
@@ -1,5 +1,37 @@
+//this is according to video tutorial
+
+//package com.example.studentmanagementsystem.service;
+//
+//import com.example.studentmanagementsystem.entity.Student;
+//
+//import java.util.List;
+//
+//public interface StudentService {
+//    List<Student> getAllStudents();
+//}
+
+
+//this is my debug process
 package com.example.studentmanagementsystem.service;
 
-public interface StudentService {
 
+import com.example.studentmanagementsystem.entity.Student;
+import com.example.studentmanagementsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public  StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
