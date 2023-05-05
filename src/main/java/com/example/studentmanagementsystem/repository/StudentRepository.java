12571f982e5b1a7d8d198931bb3146ccdbd4b251
cuongnguyen-oceansmart <cuongnguyen3900@gmail.com>
2023-05-05 18:28:04 +0700

    Switch back to using MySQL. Aiming to create other table such as ListTeachers as well.

--- src/main/java/com/example/studentmanagementsystem/repository/StudentRepository.java
+++ src/main/java/com/example/studentmanagementsystem/repository/StudentRepository.java
@@ -1,10 +1,8 @@
 package com.example.studentmanagementsystem.repository;
 
 import com.example.studentmanagementsystem.entity.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface StudentRepository extends MongoRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
--- src/main/java/com/example/studentmanagementsystem/service/StudentService.java
+++ src/main/java/com/example/studentmanagementsystem/service/StudentService.java
@@ -1,61 +1,43 @@
 //this is according to video tutorial
 
-//package com.example.studentmanagementsystem.service;
-//
-//import com.example.studentmanagementsystem.entity.Student;
-//
-//import java.util.List;
-//
-//public interface StudentService {
-//    List<Student> getAllStudents();
-//}
-
-
-//this is my debug process
 package com.example.studentmanagementsystem.service;
 
-
 import com.example.studentmanagementsystem.entity.Student;
-import com.example.studentmanagementsystem.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class StudentService {
-    private final StudentRepository studentRepository;
+public interface StudentService {
+    List<Student> getAllStudents();
 
+    Student saveStudent(Student student);
 
-    @Autowired
-    public  StudentService(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
-
-    public List<Student> getAllStudents() {
-        return studentRepository.findAll();
-    }
+    Student getStudentById(Long id);
+    Student updateStudent(Student student);
+    void deleteStudentById(Long id);
 }
 
 
-
-
+//this is my debug process
+//package com.example.studentmanagementsystem.service;
+//
+//
+//import com.example.studentmanagementsystem.entity.Student;
+//import com.example.studentmanagementsystem.repository.StudentRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
 //@Service
 //public class StudentService {
-//
-//    private final StudentRepository studentMongoRepository;
-////    private final StudentJpaRepository studentJpaRepository;
+//    private final StudentRepository studentRepository;
 //
 //    @Autowired
-//    public StudentService(@Qualifier("studentMongoRepository") StudentRepository studentMongoRepository)
-////                     @Qualifier("studentJpaRepository") StudentJpaRepository studentJpaRepository)
-//    {
-//        this.studentMongoRepository = studentMongoRepository;
-////        this.studentJpaRepository = studentJpaRepository;
+//    public  StudentService(StudentRepository studentRepository) {
+//        this.studentRepository = studentRepository;
 //    }
-//        public List<Student> getAllStudents() {
-//        return studentMongoRepository.findAll();
+//
+//    public List<Student> getAllStudents() {
+//        return studentRepository.findAll();
 //    }
 //}
-
